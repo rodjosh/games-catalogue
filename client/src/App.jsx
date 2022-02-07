ef8a9ac4c3eff3372f8dc23227ceba769fcff413
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2022-02-07 11:20:06 -0600

    Add explanatory comments to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,11 @@ import Footer from "./components/Footer";
 import Games from "./components/Games";
 
 function App() {
+
+	//Declaring page value in state
 	const [page, setPage] = useState('/');
 
+	//Returning page structure
 	return (<div className="container mx-auto">
 		<Nav setPage={setPage}/>
 		<Games page={page} />
