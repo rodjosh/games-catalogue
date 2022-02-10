4572892509e35a90d401975cbe4c65df95bdb16b
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2022-02-09 18:24:08 -0600

    Add check functionality to jwt

--- server/build/jwt.js
+++ server/build/jwt.js
@@ -8,8 +8,20 @@ const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const JWT_SECRET = env_1.default === null || env_1.default === void 0 ? void 0 : env_1.default.JWT_SECRET;
 exports.default = {
     checkJWT(req, res, next) {
+        if (req.cookies.user_token) {
+            jsonwebtoken_1.default.verify(req.cookies.user_token, JWT_SECRET, (err, decoded) => {
+                if (err)
+                    throw new Error("Invalid token");
+                const payload = decoded;
+                res.locals.username = payload.user;
+                next();
+            });
+        }
+        else {
+            throw new Error("Not authorized to view this content");
+        }
     },
     createJWT(payload) {
-        return jsonwebtoken_1.default.sign(payload, JWT_SECRET);
+        return jsonwebtoken_1.default.sign(payload, JWT_SECRET, { expiresIn: "24h" });
     }
 };
--- server/src/jwt.ts
+++ server/src/jwt.ts
@@ -1,5 +1,6 @@
 import env from "./env";
 import jwt from "jsonwebtoken";
+import type {VerifyErrors, JwtPayload} from "jsonwebtoken";
 import type {Request, Response, NextFunction} from "express";
 
 const JWT_SECRET = env?.JWT_SECRET;
@@ -10,9 +11,21 @@ interface Payload {
 
 export default {
 	checkJWT(req: Request, res: Response, next: NextFunction){
+		if (req.cookies.user_token){
+			jwt.verify(req.cookies.user_token, JWT_SECRET, (err, decoded)=>{
+				if (err) throw new Error("Invalid token");
+
+				const payload = decoded as Payload;
+				res.locals.username = payload.user;
+				next();
+			})
+			
+		} else {
+			throw new Error("Not authorized to view this content");
+		}
 		
 	},
 	createJWT(payload: Payload){
-		return jwt.sign(payload, JWT_SECRET);
+		return jwt.sign(payload, JWT_SECRET, {expiresIn: "24h"});
 	}
 }
