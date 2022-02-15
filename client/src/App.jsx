858086ebf5721dd453704119021ff0ec8ea70663
Joshua Rodriguez <joshua.david.rodriguez.medrano@gmail.com>
2022-02-15 15:57:46 -0600

    Improve code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,54 +1,30 @@
-import {useState, useEffect} from "react";
-import {Routes, Route} from "react-router-dom";
+import {useState, createContext} from "react";
+import {Routes, Route, Navigate} from "react-router-dom";
 
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Games from "./components/Games";
 import NotLogged from "./components/NotLogged";
 
-async function checkLogin(setLoggedStatus) {
-	let res;
-
-	try {
-		res = await fetch('http://localhost:3001/api/checklogin');
-	} catch {
-		console.error("Failed to request server");
-		return;
-	}
-
-	if (res.ok){
-		setLoggedStatus(true);
-	} else {
-		const text = await res.text();
-		console.log(text);
-
-		setLoggedStatus(false);
-	}
-}
+export const userToken = createContext(null);
 
 function App() {
-	const [loggedStatus, setLoggedStatus] = useState(null);
-
-	const loggedPage = (<>
+	const [user_token, setUserToken] = useState(null);
+	
+	const loggedPage = (<userToken.Provider value={user_token}>
 		<Nav />
 		<Routes>
 			<Route path="/" element={<Games />} />
 			<Route path="/genre/:genre" element={<Games />} />
+			<Route path="*" element={<Navigate to="/" replace={true} />} />
 		</Routes>
 		<Footer />
-	</>);
-
-	const checkingLoggedStatusPage = (<h1>Checking logged status</h1>);
-
-	useEffect(()=>{
-		checkLogin(setLoggedStatus);
-	}, []);
+	</userToken.Provider>);
 
 	return (<div className="container mx-auto">
 		{
-			loggedStatus === null ? checkingLoggedStatusPage : 
-				loggedStatus === true ? loggedPage : 
-					<NotLogged setLoggedStatus={setLoggedStatus} />
+			user_token !== null ? loggedPage : 
+					<NotLogged setUserToken={setUserToken} />
 		}
 	</div>);
 }
